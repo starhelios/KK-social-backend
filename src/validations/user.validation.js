0506019ad77ab36b560c88f4b2c8eab1d3f3f513
starhelios <starhelios94@gmail.com>
2021-01-13 04:52:04 -0500

    0113_fix

--- src/validations/user.validation.js
+++ src/validations/user.validation.js
@@ -37,22 +37,22 @@ const updateUser = {
   params: Joi.object().keys({
     userId: Joi.required().custom(objectId),
   }),
-  body: Joi.object()
-    .keys({
-      email: Joi.string().email(),
-      password: Joi.string().custom(password),
-      dateOfBirth: Joi.date(),
-      fullname: Joi.string(),
-      avatarUrl: Joi.string(),
-      categoryName: Joi.string(),
-      aboutMe: Joi.string(),
-      isHost: Joi.boolean(),
-      joinDay: Joi.date(),
-      availableMethods: Joi.array(),
-      stripeCustomerID: Joi.string(),
-      stripeConnectID: Joi.string(),
-    })
-    .min(1),
+  // body: Joi.object()
+  //   .keys({
+  //     email: Joi.string().email(),
+  //     password: Joi.string().custom(password),
+  //     dateOfBirth: Joi.date(),
+  //     fullname: Joi.string(),
+  //     avatarUrl: Joi.string(),
+  //     categoryName: Joi.string(),
+  //     aboutMe: Joi.string(),
+  //     isHost: Joi.boolean(),
+  //     joinDay: Joi.date(),
+  //     availableMethods: Joi.array(),
+  //     stripeCustomerID: Joi.string(),
+  //     stripeConnectID: Joi.string(),
+  //   })
+  //   .min(1),
 };
 
 const deleteUser = {
