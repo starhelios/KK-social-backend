5064e30433231199c00b7d78bba35dd62fb6bbfe
starhelios <starhelios94@gmail.com>
2021-02-09 10:30:30 -0500

    0209-1

--- src/controllers/search.controller.js
+++ src/controllers/search.controller.js
@@ -24,26 +24,24 @@ const search = catchAsync(async (req, res) => {
   if (categoryName.length > 0) {
     query.categoryName = { $in: categoryName };
   }
-
   if (startDay && endDay) {
     query.startDay = { $lte: startDay };
     query.endDay = { $gte: endDay };
   }
-  
   if (minPrice && maxPrice) {
     query.price = { $gte: minPrice, $lte: maxPrice };
   }
-
-  if (location.length > 0) {
-    query.location = { $regex: new RegExp(`.*${location}.*`, 'i') };
-  }
-
+  // if (location.length > 0) {
+  //   query.location = { $regex: new RegExp(`.*${location}.*`, 'i') };
+  // }
   const catetories = await Experience.find(query).exec();
 
   const experiences = catetories.filter((item) => {
     const today = new Date();
     const eDay = new Date(item.endDay);
     return eDay >= today;
+  }).filter((item) => {
+    return users.findIndex((u) => u.id == item.userId) >= 0;
   });
 
   res.send(generateResponse(true, { hosts: users, experiences }));
