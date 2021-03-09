06e4a97114fc2ac812dc6ce1c3df1914c3f13b6c
starhelios <starhelios94@gmail.com>
2021-03-09 11:11:15 -0500

    fix card add error

--- src/utils/photoUpload.js
+++ src/utils/photoUpload.js
@@ -7,12 +7,18 @@ const uploader = multer({
     files: 1,
   },
 });
+
+// dragon_test
+// const bucket = '';
+// const storage = '';
+
 const storage = new Storage({
   projectId: process.env.GCLOUD_PROJECT_ID,
   keyFilename: process.env.GCLOUD_APPLICATION_CREDENTIALS,
 });
 
 const bucket = storage.bucket(process.env.GCLOUD_STORAGE_BUCKET_URL);
+
 
 module.exports = {
   bucket,
